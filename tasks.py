6037327b41f17f2106f8f947c6a6178fd7eeab79
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-11-24 16:19:30 -0500

    wip: build and release tasks

--- tasks.py
+++ tasks.py
@@ -1,3 +1,5 @@
+import os
+
 from invoke import task
 
 
@@ -25,3 +27,36 @@ def test(ctx, update_snapshots=False):
         pty=True,
     )
 
+
+@task
+def build(ctx):
+    ctx.run("python setup.py sdist bdist_wheel")
+
+
+@task
+def publish(ctx, dry_run=True):
+    # ctx.run(f"twine upload {'--repository-url https://test.pypi.org/legacy/' if dry_run else ''} dist/*")
+    pass
+
+
+@task(pre=[clean, build])
+def release(ctx, dry_run=True):
+    if not dry_run and not os.environ.get("CI"):
+        ctx.run("This is a CI only command")
+        return
+
+    # get version
+    with open("version.txt", "r") as f:
+        version = str(f.read())
+
+    should_release_to_prod = (
+        not dry_run
+        and semver.parse_version_info(version)
+    )
+
+    # publish to test for dry run
+    publish(ctx, dry_run=True)
+
+    # publish to prod if test succeeds
+    if should_release_to_prod:
+        publish(ctx, dry_run=False)
