e9de9101969a018f49b0a4292509c368d3349450
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2019-11-26 13:43:13 -0500

    ci: stub out release command (#7)
    * wip: add deps
    
    * wip: export version
    
    * wip: lint root py files
    
    * wip: add wheel to dev deps
    
    * wip: add missing meta
    
    * wip: build and release tasks
    
    * wip: print publish command
    
    * wip: handle non semver tags
    
    * wip: print ci only
    
    * wip: get long description from readme
    
    * wip: only publish to test on specific version build
    
    * wip: only build in none release version

--- setup.py
+++ setup.py
@@ -8,19 +8,29 @@ python_requires = "~=3.6"
 if sys.version_info[0] == 2:
     raise Exception("Only python 3 supported.")
 
+
+def readme() -> str:
+    with open("README.md") as f:
+        return f.read()
+
+
 if __name__ == "__main__":
     setup(
         name="syrupy",
         description="PyTest Snapshot Test Utility",
         author="Top Hat Open Source",
-        use_scm_version=True,
+        author_email="opensource@tophat.com",
+        url="git@github.com:tophat/syrupy.git",
+        long_description=readme(),
+        long_description_content_type="text/markdown",
+        use_scm_version={"write_to": "version.txt"},
         package_dir={"": "src"},
         packages=["syrupy"],
         py_modules=["syrupy"],
         zip_safe=False,
         install_requires=[],
-        setup_requires=[],
-        entry_points={"pytest11": ["syrupy = syrupy"],},
+        setup_requires=["setuptools_scm"],
+        entry_points={"pytest11": ["syrupy = syrupy"]},
         python_requires=python_requires,
         classifiers=["Framework :: Pytest"],
     )
--- tasks.py
+++ tasks.py
@@ -1,3 +1,6 @@
+import os
+
+import semver
 from invoke import task
 
 
@@ -31,3 +34,38 @@ def test(ctx, update_snapshots=False):
         f"python -m pytest . {'--update-snapshots' if update_snapshots else ''}",
         pty=True,
     )
+
+
+@task(pre=[clean])
+def build(ctx):
+    ctx.run("python setup.py sdist bdist_wheel")
+
+
+@task
+def publish(ctx, dry_run=True):
+    print(
+        f"twine upload {'--repository-url https://test.pypi.org/legacy/' if dry_run else ''} dist/*"
+    )
+
+
+@task(pre=[build])
+def release(ctx, dry_run=True):
+    if not dry_run and not os.environ.get("CI"):
+        print("This is a CI only command")
+        exit(1)
+
+    # get version created in build
+    with open("version.txt", "r") as f:
+        version = str(f.read())
+
+    try:
+        should_publish_to_pypi = not dry_run and semver.parse_version_info(version)
+    except ValueError:
+        should_publish_to_pypi = False
+
+    # publish to test to verify builds
+    publish(ctx, dry_run=True)
+
+    # publish to pypi if test succeeds
+    if should_publish_to_pypi:
+        publish(ctx, dry_run=False)
