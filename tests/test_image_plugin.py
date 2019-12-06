7be04de8e10c7059423ddcf6f4287c9e65821eed
Emmanuel Ogbizi <emmanuel.ogbizi-ugbe@tophatmonocle.com>
2019-12-06 10:37:11 -0500

    ci: add ci test coverage flags

--- tests/test_image_plugin.py
+++ tests/test_image_plugin.py
@@ -1,7 +1,7 @@
 import base64
 import pytest
 
-from syrupy.plugins.image import PNGImageSnapshotIO, ImageSnapshotSerializer
+from src.syrupy.plugins.image import PNGImageSnapshotIO, ImageSnapshotSerializer
 
 
 @pytest.fixture
