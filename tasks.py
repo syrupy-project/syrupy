fbcccc4d83fa4a2fe9bb19b00c0ad58c81b318ab
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2019-12-11 08:21:59 -0500

    ci: set up coverage (#35)
    * ci: set up coverage
    
    * docs: coverage badge

--- tasks.py
+++ tasks.py
@@ -6,16 +6,25 @@ from invoke import task
 
 @task
 def clean(ctx):
+    """
+    Remove build files e.g. package, distributable, compiled etc.
+    """
     ctx.run(f"rm -rf *.egg-info dist build __pycache__ .pytest_cache artifacts/*")
 
 
 @task
 def requirements(ctx):
+    """
+    Build requirements lock file
+    """
     ctx.run(f"python -m piptools compile dev-requirements.in", pty=True)
 
 
 @task
 def lint(ctx, fix=False):
+    """
+    Check and fix syntax
+    """
     lint_commands = {
         "mypy": "python -m mypy --strict src",
         "isort": f"python -m isort {'' if fix else '--check-only --diff'} -y",
@@ -29,23 +38,51 @@ def lint(ctx, fix=False):
 
 
 @task
-def test(ctx, snapshot_update=False, verbose=False):
-    ctx.run(
-        "python -m pytest ."
-        f"{' -s' if verbose else ''}"
-        f"{' --snapshot-update' if snapshot_update else ''}",
-        env={"PYTHONPATH": "./src"},
-        pty=True,
-    )
+def install(ctx):
+    """
+    Install the current development version of syrupy
+    """
+    ctx.run("python -m pip install -U .", pty=True)
+
+
+@task(
+    help={
+        "coverage": "Build and report on test coverage",
+        "setup": "Reinstall syrupy development version",
+        "update-snapshots": "Create, update or delete snapshot files",
+        "verbose": "Verbose output e.g. non captured logs etc.",
+    }
+)
+def test(ctx, coverage=False, setup=False, update_snapshots=False, verbose=False):
+    """
+    Run entire test suite
+    """
+    if setup:
+        install(ctx)
+    flags = {
+        "-s": verbose,
+        "--cov=./src": coverage,
+        "--snapshot-update": update_snapshots,
+    }
+    test_flags = " ".join(flag for flag, enabled in flags.items() if enabled)
+    ctx.run(f"python -m pytest {test_flags} .", pty=True)
+    if coverage:
+        ctx.run("codecov", pty=True)
 
 
 @task(pre=[clean])
 def build(ctx):
+    """
+    Generate version from scm and build package distributable
+    """
     ctx.run("python setup.py sdist bdist_wheel")
 
 
 @task
 def publish(ctx, dry_run=True):
+    """
+    Upload built package to pypi
+    """
     ctx.run(
         f"twine upload {'--repository-url https://test.pypi.org/legacy/' if dry_run else ''} dist/*"
     )
@@ -53,6 +90,9 @@ def publish(ctx, dry_run=True):
 
 @task(pre=[build])
 def release(ctx, dry_run=True):
+    """
+    Build and publish package to pypi index based on scm version
+    """
     if not dry_run and not os.environ.get("CI"):
         print("This is a CI only command")
         exit(1)
