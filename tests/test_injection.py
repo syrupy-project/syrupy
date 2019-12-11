f8bb432d77d1e1ab695a0a2ea1c95d93197c9b7f
Emmanuel Ogbizi <emmanuel.ogbizi-ugbe@tophatmonocle.com>
2019-12-11 10:21:07 -0500

    refactor: move test to injection

--- tests/test_injection.py
+++ tests/test_injection.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_fixture(testdir):
     pyfile_content = """
 def test_sth(snapshot):
@@ -9,3 +12,24 @@ def test_sth(snapshot):
     result.stdout.fnmatch_lines(["*::test_sth PASSED*"])
 
     assert result.ret == 0
+
+
+@pytest.fixture
+def snapshot_atomic_write(snapshot):
+    previous_count = snapshot.num_executions
+    previous_update_snapshots = snapshot._update_snapshots
+    snapshot._update_snapshots = True
+    yield snapshot
+    for i in range(previous_count, snapshot.num_executions):
+        snapshot.serializer.write(None, i)
+    snapshot._update_snapshots = previous_update_snapshots
+
+
+def test_written_snapshots(snapshot_atomic_write):
+    snapshot_data = "This snapshot should not be committed"
+    assert snapshot_data == snapshot_atomic_write
+    snapshot_file = snapshot_atomic_write.serializer.get_filepath(
+        snapshot_atomic_write.num_executions
+    )
+    with open(snapshot_file, "r") as f:
+        assert snapshot_data in f.read()
--- tests/test_snapshots.py
+++ tests/test_snapshots.py
@@ -3,17 +3,6 @@ from collections import namedtuple
 import pytest
 
 
-@pytest.fixture
-def snapshot_atomic_write(snapshot):
-    update_snapshots = snapshot._update_snapshots
-    snapshot._update_snapshots = True
-    previous_count = snapshot.num_executions
-    yield snapshot
-    for i in range(previous_count + 1, snapshot.num_executions):
-        snapshot.serializer.write(None, i)
-    snapshot._update_snapshots = update_snapshots
-
-
 def test_simple_string(snapshot):
     assert "Loreeeeeem ipsum." == snapshot
 
@@ -35,16 +24,6 @@ def test_multiple_snapshots(snapshot):
 def test_missing_snapshots(snapshot):
     with pytest.raises(AssertionError, match="Snapshot does not exist"):
         assert "This snapshot should not be written" == snapshot
-
-
-def test_written_snapshots(snapshot_atomic_write):
-    snapshot_data = "This snapshot should not be committed"
-    assert snapshot_data == snapshot_atomic_write
-    snapshot_file = snapshot_atomic_write.serializer.get_filepath(
-        snapshot_atomic_write.num_executions
-    )
-    with open(snapshot_file, "r") as f:
-        assert snapshot_data in f.read()
 
 
 @pytest.mark.parametrize("expected", [r"Escaped \n", r"Backslash \u U"])
