bb23fb25a75788b2527c5464fb398133e6840370
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-12-12 02:29:03 -0500

    test: deletion of files

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,58 +1,60 @@
+import os
+
 import pytest
 
 
-def test_fixture(testdir):
-    pyfile_content = """
-def test_sth(snapshot):
-    assert snapshot is not None
-"""
-    testdir.makepyfile(pyfile_content)
+@pytest.fixture
+def stubs(testdir):
+    tests = {
+        "inject": """def test_injection(snapshot):
+    assert snapshot is not None""",
+        "used": """def test_used(snapshot):
+    assert snapshot == 'used'""",
+        "unused": """def test_unused(snapshot):
+    assert snapshot == 'unused'""",
+    }
+    pyfile_content = "\n\n".join(tests.values())
+    testdir.makepyfile(test_file=pyfile_content)
+    filepath = os.path.join(testdir.tmpdir, "__snapshots__", "test_file.yaml")
+    return testdir.runpytest("-v", "--snapshot-update"), testdir, tests, filepath
 
-    result = testdir.runpytest("-v")
-    result.stdout.fnmatch_lines(["*::test_sth PASSED*"])
 
+def test_fixture(stubs):
+    result = stubs[0]
+    result.stdout.fnmatch_lines(["*::test_injection PASSED*"])
     assert result.ret == 0
 
 
-def test_unused_snapshots(testdir):
-    pyfile_used_snapshot = """
-def test_used(snapshot):
-    assert snapshot == 'this snapshot is used'
-"""
-    pyfile_content = f"""{pyfile_used_snapshot}
-def test_unused(snapshot):
-    assert snapshot == 'this snapshot will be unused'
-"""
-    testdir.makepyfile(test_file=pyfile_content)
-    result = testdir.runpytest("-v", "--snapshot-update")
+def test_unused_snapshots(stubs):
+    result, testdir, tests, _ = stubs
     assert "2\x1b[0m snapshots generated" in result.stdout.str()
     assert "0\x1b[0m snapshots unused" in result.stdout.str()
     assert result.ret == 0
 
-    testdir.makepyfile(test_file=pyfile_used_snapshot)
+    testdir.makepyfile(test_file="\n\n".join(tests[k] for k in tests if k != "unused"))
     result = testdir.runpytest("-v")
     assert "snapshots generated" not in result.stdout.str()
     assert "1\x1b[0m snapshot unused" in result.stdout.str()
     assert result.ret == 0
 
 
-@pytest.fixture
-def snapshot_atomic(snapshot):
-    previous_count = snapshot.num_executions
-    previous_update_snapshots = snapshot._update_snapshots
-    snapshot._update_snapshots = True
-    yield snapshot
-    for i in range(previous_count, snapshot.num_executions):
-        snapshot.serializer.write(None, i)
-    snapshot._update_snapshots = previous_update_snapshots
-    snapshot._executions = previous_count
+def test_removed_snapshots(stubs):
+    _, testdir, tests, filepath = stubs
+    assert os.path.isfile(filepath)
+    testdir.makepyfile(test_file="\n\n".join(tests[k] for k in tests if k != "unused"))
+    result = testdir.runpytest("-v", "--snapshot-update")
+    assert "1\x1b[0m snapshot unused" in result.stdout.str()
+    assert "This snapshot has been deleted" in result.stdout.str()
+    assert result.ret == 0
+    assert os.path.isfile(filepath)
 
 
-def test_written_snapshots(snapshot_atomic):
-    snapshot_data = "This snapshot should not be committed"
-    assert snapshot_data == snapshot_atomic
-    snapshot_file = snapshot_atomic.serializer.get_filepath(
-        snapshot_atomic.num_executions
-    )
-    with open(snapshot_file, "r") as f:
-        assert snapshot_data in f.read()
+def test_removed_snapshot_file(stubs):
+    _, testdir, tests, filepath = stubs
+    assert os.path.isfile(filepath)
+    testdir.makepyfile(test_file=tests["inject"])
+    result = testdir.runpytest("-v", "--snapshot-update")
+    assert "2\x1b[0m snapshots unused" in result.stdout.str()
+    assert "These snapshots have been deleted" in result.stdout.str()
+    assert result.ret == 0
+    assert not os.path.isfile(filepath)
