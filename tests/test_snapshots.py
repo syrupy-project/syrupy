01fb4b9574dc0833624f81f7f9df46718515bead
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-12-24 17:25:04 -0500

    test: class names

--- tests/test_snapshots.py
+++ tests/test_snapshots.py
@@ -47,3 +47,8 @@ ExampleTuple = namedtuple("ExampleTuple", ["a", "b", "c", "d"])
 def test_tuples(snapshot):
     assert snapshot == ("this", "is", ("a", "tuple"))
     assert snapshot == ExampleTuple(a="this", b="is", c="a", d={"named", "tuple"})
+
+
+class TestClass:
+    def test_name(self, snapshot):
+        assert snapshot == "this is in a test class"
