63a027cd44cbd53447ddb67569a7de0d2259d083
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-12-30 21:52:51 -0500

    test: raw single snapshot serializer does not write non binary

---
+++ tests/test_raw_single_serializer.py
@@ -0,0 +1,29 @@
+import os
+from typing import TYPE_CHECKING
+
+import pytest
+
+from syrupy.data import (
+    SnapshotData,
+    SnapshotFile,
+)
+from syrupy.serializers.raw_single import RawSingleSnapshotSerializer
+
+
+if TYPE_CHECKING:
+    from syrupy.assertion import SnapshotAssertion
+
+
+@pytest.fixture
+def snapshot_raw(snapshot):
+    return snapshot.with_class(serializer_class=RawSingleSnapshotSerializer)
+
+
+def test_does_not_write_non_binary(testdir, snapshot_raw: "SnapshotAssertion"):
+    snapshot_file = SnapshotFile(
+        filepath=os.path.join(testdir.tmpdir, "snapshot_file.raw"),
+        snapshots={"snapshot_data": SnapshotData(data="non binary data")},
+    )
+    with pytest.raises(TypeError):
+        snapshot_raw.serializer._write_snapshot_to_file(snapshot_file)
+    assert not os.path.exists(snapshot_file.filepath)
