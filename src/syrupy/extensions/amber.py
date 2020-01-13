ce5a1c56cb79163fe7ccbf2202e08a58ae44649b
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2020-01-13 18:18:15 -0500

    feat: update serialization to support named tuple fields (#108)
    * fix: add field support for namedtuple serialization
    
    * fix: add trailing commas to class field serialization
    
    * test: update snapshots
    
    * chore: update changelog
    
    * refactor: named_tuple is namedtuple

--- src/syrupy/extensions/amber.py
+++ src/syrupy/extensions/amber.py
@@ -159,6 +159,33 @@ class DataSerializer:
         )
 
     @classmethod
+    def __is_namedtuple(cls, obj: Any) -> bool:
+        return isinstance(obj, tuple) and all(
+            type(n) == str for n in getattr(obj, "_fields", [None])
+        )
+
+    @classmethod
+    def serialize_namedtuple(
+        cls, data: Any, *, depth: int = 0, visited: Optional[Set[Any]] = None
+    ) -> str:
+        return (
+            cls.with_indent(f"{cls.object_type(data)} (\n", depth)
+            + "".join(
+                f"{serialized_key}={serialized_value.lstrip(cls._indent)},\n"
+                for serialized_key, serialized_value in (
+                    (
+                        cls.with_indent(name, depth=depth + 1),
+                        cls.serialize(
+                            data=getattr(data, name), depth=depth + 1, visited=visited
+                        ),
+                    )
+                    for name in cls.sort(data._fields)
+                )
+            )
+            + cls.with_indent(")", depth)
+        )
+
+    @classmethod
     def serialize_iterable(
         cls,
         data: "SerializableData",
@@ -189,7 +216,7 @@ class DataSerializer:
         return (
             cls.with_indent(f"{cls.object_type(data)} {{\n", depth)
             + "".join(
-                f"{serialized_key}={serialized_value.lstrip(cls._indent)}\n"
+                f"{serialized_key}={serialized_value.lstrip(cls._indent)},\n"
                 for serialized_key, serialized_value in (
                     (
                         cls.with_indent(name, depth=depth + 1),
@@ -231,6 +258,8 @@ class DataSerializer:
             serialize_method = cls.serialize_set
         elif isinstance(data, dict):
             serialize_method = cls.serialize_dict
+        elif cls.__is_namedtuple(data):
+            serialize_method = cls.serialize_namedtuple
         elif isinstance(data, (list, tuple, GeneratorType)):
             serialize_method = cls.serialize_iterable
         return serialize_method(**serialize_kwargs)
