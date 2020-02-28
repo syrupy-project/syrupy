a69668173aed4eac86a0d7a14c6c9d647c6dbfc2
Emmanuel Ogbizi <emmanuel.ogbizi-ugbe@tophatmonocle.com>
2020-02-28 16:20:41 -0500

    refactor: account for matching parameterized names

--- src/syrupy/location.py
+++ src/syrupy/location.py
@@ -23,15 +23,11 @@ class TestLocation(object):
     def filename(self) -> str:
         return Path(self.filepath).stem
 
-    def __matches_snapshot_name(self, var_name: str, snapshot_name: str) -> bool:
-        if var_name.isidentifier():
-            return var_name in snapshot_name
-        return snapshot_name in var_name
+    def __pick_id(self, name: str) -> str:
+        return "".join(c for c in name if c.isidentifier())
 
     def matches_snapshot_name(self, snapshot_name: str) -> bool:
-        return self.__matches_snapshot_name(
-            str(self.methodname), snapshot_name
-        ) or self.__matches_snapshot_name(str(self.nodename), snapshot_name)
+        return self.__pick_id(str(self.methodname)) == self.__pick_id(snapshot_name)
 
     def matches_snapshot_location(self, snapshot_location: str) -> bool:
         return self.filename in snapshot_location
