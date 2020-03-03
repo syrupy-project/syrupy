96b4914934bc2b4d48fa6ff720f1b7a51877957b
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-03-02 23:48:13 -0500

    ci: semantic release action

---
+++ .releaserc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    branch: "master",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/changelog",
+        "@semantic-release/git",
+        "@semantic-release/github",
+    ],
+};
