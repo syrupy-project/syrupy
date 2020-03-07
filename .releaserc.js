c93103e1c399d762df87b64ebc524c2619351f8d
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2020-03-06 20:47:36 -0500

    ci: automate semantic releases (#148)
    * ci: semantic release action
    
    * chore: replace semantic-release action
    
    * wip: test semantic release dryrun
    
    * wip: move config to workflow
    
    * chore: set branch to master
    
    * chore: dry run for now

---
+++ .releaserc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/changelog",
+        "@semantic-release/git",
+        "@semantic-release/github",
+    ],
+};
