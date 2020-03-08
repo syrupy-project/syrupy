acd7ba995db5f901ae9f6ad8b7fddd2f12bcd3b7
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-03-07 20:49:52 -0500

    chore: semantic release title

--- .releaserc.js
+++ .releaserc.js
@@ -6,7 +6,7 @@ module.exports = {
             "@semantic-release/changelog",
             {
                 changelogTitle:
-                    "# Changelog\n\nAll notable changes to this project will be documented in this file.\n\nThe format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).\n\nFrom v1.0.0 onwards, this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html). Pre-v1, breaking changes will be indicated via a minor release, while all other changes will fall under patches. At any time, you can see what's in progress for a version by filtering GitHub issues by milestone.\n",
+                    "# Changelog\n\nAll notable changes to this project will be documented in this file.\n\nThe format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).\n\nFrom v1.0.0 onwards, this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html). Pre-v1, breaking changes will be indicated via a minor release, while all other changes will fall under patches. At any time, you can see what's in progress for a version by filtering GitHub issues by milestone.",
             },
         ],
         ["@semantic-release/git", { assets: ["CHANGELOG.md"] }],
