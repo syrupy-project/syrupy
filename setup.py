921c71a5af482b7b2a9d9c415e61a092b76d7d9a
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2020-03-10 22:47:03 +0000

    fix: specify correct min version of pytest

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = [
     "attrs>=18.2.0,<20.0.0",
     "colored>=1.3.92,<2.0.0",
     "typing_extensions>=3.6,<4.0.0",
-    "pytest>=5.0.0,<6.0.0",
+    "pytest>=5.1.0,<6.0.0",
 ]
 test_requires = [
     "codecov",
