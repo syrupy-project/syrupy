14c822c9d931d8b7b74d6ba2a5fc8604ee8f4daf
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-03-20 20:25:11 -0400

    test: add example

--- tests/test_extension_image.py
+++ tests/test_extension_image.py
@@ -8,19 +8,32 @@ from syrupy.extensions.image import (
 )
 
 
+actual_png = base64.b64decode(
+    b"iVBORw0KGgoAAAANSUhEUgAAADIAAAAyBAMAAADsEZWCAAAAG1BMVEXMzMy"
+    b"Wlpaqqqq3t7exsbGcnJy+vr6jo6PFxcUFpPI/AAAACXBIWXMAAA7EAAAOxA"
+    b"GVKw4bAAAAQUlEQVQ4jWNgGAWjgP6ASdncAEaiAhaGiACmFhCJLsMaIiDAE"
+    b"QEi0WXYEiMCOCJAJIY9KuYGTC0gknpuHwXDGwAA5fsIZw0iYWYAAAAASUVO"
+    b"RK5CYII="
+)
+actual_svg = (
+    '<?xml version="1.0" encoding="UTF-8"?>'
+    '<svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">'
+    '<g><rect width="50" height="50" fill="#fff"/>'
+    '<g><g fill="#fff" stroke="#707070">'
+    '<rect width="50" height="50" stroke="none"/>'
+    '<rect x="0" y="0" width="50" height="50" fill="none"/></g>'
+    '<text transform="translate(10 27)" fill="#707070" '
+    'font-family="ConsolasForPowerline, Consolas for Powerline" font-size="8">'
+    '<tspan x="0" y="0">50 x 50</tspan></text></g></g></svg>'
+)
+
+
 @pytest.fixture
 def snapshot_png(snapshot):
     return snapshot.use_extension(PNGImageSnapshotExtension)
 
 
 def test_image(snapshot_png):
-    actual_png = base64.b64decode(
-        b"iVBORw0KGgoAAAANSUhEUgAAADIAAAAyBAMAAADsEZWCAAAAG1BMVEXMzMy"
-        b"Wlpaqqqq3t7exsbGcnJy+vr6jo6PFxcUFpPI/AAAACXBIWXMAAA7EAAAOxA"
-        b"GVKw4bAAAAQUlEQVQ4jWNgGAWjgP6ASdncAEaiAhaGiACmFhCJLsMaIiDAE"
-        b"QEi0WXYEiMCOCJAJIY9KuYGTC0gknpuHwXDGwAA5fsIZw0iYWYAAAAASUVO"
-        b"RK5CYII="
-    )
     assert actual_png == snapshot_png
 
 
@@ -28,15 +41,15 @@ def test_image_vector(snapshot):
     """
     Example of creating a previewable svg snapshot
     """
-    actual_svg = (
-        '<?xml version="1.0" encoding="UTF-8"?>'
-        '<svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">'
-        '<g><rect width="50" height="50" fill="#fff"/>'
-        '<g><g fill="#fff" stroke="#707070">'
-        '<rect width="50" height="50" stroke="none"/>'
-        '<rect x="0" y="0" width="50" height="50" fill="none"/></g>'
-        '<text transform="translate(10 27)" fill="#707070" '
-        'font-family="ConsolasForPowerline, Consolas for Powerline" font-size="8">'
-        '<tspan x="0" y="0">50 x 50</tspan></text></g></g></svg>'
-    )
     assert snapshot(extension_class=SVGImageSnapshotExtension) == actual_svg
+
+
+def test_multiple_snapshot_extensions(snapshot):
+    """
+    Example of switching extension classes on the fly.
+    These should be indexed in order of assertion.
+    """
+    assert actual_svg == snapshot(extension_class=SVGImageSnapshotExtension)
+    assert actual_svg == snapshot(extension_class=SVGImageSnapshotExtension)
+    assert actual_png == snapshot(extension_class=PNGImageSnapshotExtension)
+    assert actual_svg == snapshot(extension_class=SVGImageSnapshotExtension)
