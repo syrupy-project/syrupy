8f274bf2cc42c37751dbf7de4e87777dd6cccf33
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-04-06 01:15:44 -0400

    wip: pyperf command

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -4,6 +4,7 @@ from typing import (
     TYPE_CHECKING,
     Any,
     Callable,
+    Optional,
     Tuple,
 )
 
@@ -12,8 +13,12 @@ from github import Github
 
 if TYPE_CHECKING:
     from subprocess import CompletedProcess
+    from github.CommitStatus import CommitStatus
 
-OUTPUT_FILE = ".bench.perf.json"
+BENCH_COMMAND = "pytest -qq"
+BENCH_PREF_FILE = ".bench.perf.json"
+GH_REPO = "tophat/syrupy"
+GH_STATUS_CONTEXT = "ci/benchmark/syrupy"
 
 
 def report_pending(github=None) -> None:
@@ -26,25 +31,30 @@ def default_runner(cmd: str) -> "CompletedProcess":
 
 
 def measure_perf(run: Callable[..., Any] = default_runner) -> None:
-    run(
-        "python -m pyperf timeit '"
-        'import pytest; pytest.main(["-qqq"])'
-        f"' -o {OUTPUT_FILE}"
-    )
+    run(f"python -m pyperf command -o {BENCH_PREF_FILE} -- {BENCH_COMMAND}")
 
 
-def compare_status(head="master") -> Tuple[bool, str]:
+def get_branch_status(github, branch) -> Optional["CommitStatus"]:
+    head = github.get_repo(GH_REPO).get_branch(branch)
+    for status in head.commit.get_statuses():
+        if status.context == GH_STATUS_CONTEXT:
+            return status
+    return None
+
+
+def compare_status(github, head="master") -> Tuple[bool, str]:
+    head_status = get_branch_status(github, head)
     return (True, "benchmark successful")
 
 
 def report_status(github=None) -> None:
     if not github:
         return
+    status = compare_status(github)
 
 
 def main(report=False) -> None:
     github = Github(os.environ["GITHUB_TOKEN"]) if report else None
     report_pending(github)
     measure_perf()
-    compare_status(github)
     report_status(github)
