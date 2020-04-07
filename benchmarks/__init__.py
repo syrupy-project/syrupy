e5dad8ca6da8a0c3fbacea7b59e589764923e6b9
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-04-06 21:51:38 -0400

    wip: report benchmark status

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -5,6 +5,7 @@ from typing import (
     Any,
     Callable,
     Optional,
+    Tuple,
 )
 
 from github import Github  # type: ignore
@@ -18,31 +19,63 @@ if TYPE_CHECKING:
 BENCH_COMMAND = "pytest -qq"
 BENCH_PERF_FILE = ".perf.bench.json"
 BENCH_REF_FILE = ".ref.bench.json"
+GH_BRANCH_REF = "master"
 GH_REPO = "tophat/syrupy"
 GH_STATUS_CONTEXT = "ci/benchmark/syrupy"
 GH_STATUS_DESC_DELIM = "\n" * 3
 
 
-def report_pending(github: Optional["Github"] = None) -> None:
-    if not github:
-        return
-
-
-def default_runner(cmd: str) -> "CompletedProcess[bytes]":
-    return subprocess.run(cmd, shell=True, check=True)
-
-
-def measure_perf(run: Callable[..., Any] = default_runner) -> None:
-    run(f"python -m pyperf command -o {BENCH_PERF_FILE} -- {BENCH_COMMAND}")
-    run(f"python -m pyperf check {BENCH_PERF_FILE}")
-
-
 def get_commit(github: "Github") -> "Commit":
+    """
+    Get current commit the ci run is for
+    """
     github_sha = os.environ.get("GITHUB_SHA")
     return github.get_repo(GH_REPO).get_commit(github_sha) if github_sha else None
 
 
+def report_pending(github: Optional["Github"] = None) -> None:
+    """
+    Set the commit benchmark status to pending
+    """
+    if not github:
+        return
+    commit = get_commit(github)
+    commit.create_status(
+        state="pending",
+        target_url="#",
+        description="Running benchmarks",
+        context=GH_STATUS_CONTEXT,
+    )
+
+
+def default_runner(cmd: str) -> "CompletedProcess[bytes]":
+    """
+    Default code runner, interchangeable with invoke context runner
+    """
+    return subprocess.run(cmd, shell=True, check=True)
+
+
+def measure_perf(run: Callable[..., Any] = default_runner) -> None:
+    """
+    Measure benchmark command performance and save results in file
+    Also checks that the benchmark run was stable
+    """
+    run(f"python -m pyperf command -o {BENCH_PERF_FILE} -- {BENCH_COMMAND}")
+    run(f"python -m pyperf check {BENCH_PERF_FILE}")
+
+
+def get_branch() -> Optional[str]:
+    """
+    Get the current branch the ci run was for
+    """
+    github_ref = os.environ.get("GITHUB_REF")
+    return github_ref.replace("refs/heads/", "") if github_ref else None
+
+
 def get_branch_status(github: "Github", branch: str) -> "CommitStatus":
+    """
+    Get the commit status of a branch for the benchmark context
+    """
     head = github.get_repo(GH_REPO).get_branch(branch)
     for status in head.commit.get_statuses():
         if status.context == GH_STATUS_CONTEXT:
@@ -51,37 +84,67 @@ def get_branch_status(github: "Github", branch: str) -> "CommitStatus":
 
 
 def parse_status(commit_status: "CommitStatus") -> str:
+    """
+    Get the benchmark results from a github commit status
+    """
     return str(commit_status.description).split(GH_STATUS_DESC_DELIM)[1]
 
 
-def fetch_bench_ref_json(github: "Github", head: str = "master") -> bool:
+def fetch_bench_ref_json(github: "Github", head: str) -> bool:
+    """
+    Retrieve and save the benchmark results on the reference branch
+    Skipping the current branch is the reference branch
+    """
     head_status = get_branch_status(github, head)
-    if not head_status:
+    if not head_status or get_branch() == GH_BRANCH_REF:
         return False
     with open(BENCH_REF_FILE, "w") as ref_json_file:
         ref_json_file.write(parse_status(head_status))
     return True
 
 
-def compare_status(
-    run: Callable[..., Any] = default_runner,
-    *,
-    perf_file: str = BENCH_PERF_FILE,
-    ref_file: Optional[str] = BENCH_REF_FILE,
-) -> None:
-    run(f"python -m perf compare_to {ref_file} {perf_file}")
+def compare_bench_status(
+    perf_file: str = BENCH_PERF_FILE, ref_file: str = BENCH_REF_FILE,
+) -> Tuple[str, bool]:
+    """
+    Compare benchmarks for current branch to reference and return results
+    indicating success or failure and the github status description
+    """
+    results = default_runner(f"python -m pyperf compare_to -q {ref_file} {perf_file}")
+    return str(results.stdout).strip(), True
+
+
+def get_bench_status(bench_file: str) -> Tuple[str, bool]:
+    """
+    Get the status of the benchmark file indicating success or failure
+    and the github status description
+    """
+    results = default_runner(f"python -m pyperf show -q {bench_file}")
+    return str(results.stdout).strip(), True
 
 
 def report_status(github: Optional["Github"] = None) -> None:
+    """
+    Report benchmark run results to github as the commit status
+    """
     if not github:
         return
+
     commit = get_commit(github)
     if not commit:
         return
-    if fetch_bench_ref_json(github):
-        compare_status()
+
+    if fetch_bench_ref_json(github, GH_BRANCH_REF):
+        description, success = compare_bench_status()
     else:
-        compare_status(ref_file=None)
+        description, success = get_bench_status(BENCH_PERF_FILE)
+
+    commit.create_status(
+        state="success" if success else "failure",
+        target_url="#",
+        description=description,
+        context=GH_STATUS_CONTEXT,
+    )
 
 
 def main(report: bool = False) -> None:
