831e18f33630d804bf61a484a795cf7ef6122f37
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-07-12 04:34:33 -0400

    feat: add simple props filter helper

--- src/syrupy/filters.py
+++ src/syrupy/filters.py
@@ -2,11 +2,7 @@ from typing import TYPE_CHECKING
 
 
 if TYPE_CHECKING:
-    from syrupy.types import (
-        PropertyFilter,
-        PropertyName,
-        PropertyPath,
-    )
+    from syrupy.types import PropertyFilter, PropertyName, PropertyPath
 
 
 def paths(path_string: str, *path_strings: str) -> "PropertyFilter":
@@ -19,3 +15,14 @@ def paths(path_string: str, *path_strings: str) -> "PropertyFilter":
         return any(path_str == p for p in (path_string, *path_strings))
 
     return path_filter
+
+
+def props(prop_name: str, *prop_names: str) -> "PropertyFilter":
+    """
+    Factory to create filter using list of props
+    """
+
+    def prop_filter(prop: "PropertyName", path: "PropertyPath") -> bool:
+        return any(str(prop) == p for p in (prop_name, *prop_names))
+
+    return prop_filter
--- tests/test_filters.py
+++ tests/test_filters.py
@@ -2,7 +2,10 @@ import datetime
 
 import pytest
 
-from syrupy.filters import paths
+from syrupy.filters import (
+    paths,
+    props,
+)
 
 
 def test_filters_path_noop(snapshot):
@@ -10,7 +13,7 @@ def test_filters_path_noop(snapshot):
         paths()
 
 
-def test_filters_expected_path(snapshot):
+def test_filters_expected_paths(snapshot):
     actual = {
         0: "some value",
         "date": datetime.datetime.now(),
@@ -20,7 +23,25 @@ def test_filters_expected_path(snapshot):
     assert actual == snapshot(exclude=paths("0", "date", "nested.id", "list.0"))
 
 
-def test_filters_error_prop(snapshot):
+def test_filters_prop_noop(snapshot):
+    with pytest.raises(TypeError, match="required positional argument"):
+        props()
+
+
+def test_filters_expected_props(snapshot):
+    actual = {
+        0: "some value",
+        "date": datetime.datetime.now(),
+        "nested": {"id": 4, "other": "value"},
+        "list": [1, 2],
+    }
+    assert actual == snapshot(exclude=props("0", "date", "id"))
+
+
+@pytest.mark.parametrize(
+    "predicate", [paths("exclude_me", "nested.exclude_me"), props("exclude_me")]
+)
+def test_filters_error_prop(snapshot, predicate):
     class CustomClass:
         @property
         def include_me(self):
@@ -33,4 +54,4 @@ def test_filters_error_prop(snapshot):
     class WithNested(CustomClass):
         nested = CustomClass()
 
-    assert WithNested() == snapshot(exclude=paths("exclude_me", "nested.exclude_me"))
+    assert WithNested() == snapshot(exclude=predicate)
