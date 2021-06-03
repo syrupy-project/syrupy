373740575cefa39479f6db5992d6b363599b131f
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2021-06-02 20:45:39 -0400

    test: rupdate extension test too not use item from parent

--- tests/integration/test_pytest_extension.py
+++ tests/integration/test_pytest_extension.py
@@ -6,33 +6,22 @@ pytest_version = tuple(int(v) for v in pytest.__version__.split("."))
 @pytest.fixture
 def run_testcases(testdir):
     conftest = """
-        import pytest
-
-        class CustomItem(pytest.Item, pytest.File):
-            def __init__(self, *args, fspath, parent, **kwargs):
-                super().__init__(fspath, parent=parent)
-                self._nodeid += "::CUSTOM"
-
-            def runtest(self):
-                pass
-
-        def pytest_collect_file(path, parent):
-            return CustomItem.from_parent(fspath=path, parent=parent)
+        def pytest_itemcollected(item):
+            item._nodeid += "::CUSTOM"
         """
     testcase = """
         def test_example(snapshot):
             assert snapshot == 1
         """
-    testdir.makepyfile(conftest=conftest)
+    testdir.makeconftest(conftest)
     testdir.makepyfile(test_file=testcase)
     return testdir
 
 
-@pytest.mark.skipif(pytest_version < (6, 0), reason="at least pytest 6 required")
 def test_ignores_non_function_nodes(run_testcases):
     testdir = run_testcases
     result = testdir.runpytest("test_file.py", "-v", "--snapshot-update")
-    result.stdout.re_match_lines((r".*test_file.py::CUSTOM.*"))
+    result.stdout.re_match_lines((r".*test_file.*::CUSTOM.*"))
     assert result.ret == 0
 
 
