2403eacf32f15a8833b2f9c21e0df04486bf8085
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2021-06-02 20:17:57 -0400

    test: handle case where both pyargs and test file given

--- tests/integration/test_pytest_extension.py
+++ tests/integration/test_pytest_extension.py
@@ -3,8 +3,8 @@ import pytest
 pytest_version = tuple(int(v) for v in pytest.__version__.split("."))
 
 
-@pytest.mark.skipif(pytest_version < (6, 0), reason="at least pytest 6 required")
-def test_ignores_non_function_nodes(testdir):
+@pytest.fixture
+def run_testcases(testdir):
     conftest = """
         import pytest
 
@@ -25,6 +25,19 @@ def test_ignores_non_function_nodes(testdir):
         """
     testdir.makepyfile(conftest=conftest)
     testdir.makepyfile(test_file=testcase)
+    return testdir
+
+
+@pytest.mark.skipif(pytest_version < (6, 0), reason="at least pytest 6 required")
+def test_ignores_non_function_nodes(run_testcases):
+    testdir = run_testcases
     result = testdir.runpytest("test_file.py", "-v", "--snapshot-update")
     result.stdout.re_match_lines((r".*test_file.py::CUSTOM.*"))
+    assert result.ret == 0
+
+
+def test_handles_pyargs_non_module_when_both_given(run_testcases):
+    result = run_testcases.runpytest(
+        "-v", "test_file.py", "--pyargs", "test_file", "--snapshot-update"
+    )
     assert result.ret == 0
