77ae0abc172af7f76b5b967f5394a6eaa0ab3276
noahnu <noahnu@gmail.com>
2021-10-22 13:46:18 -0400

    test: add coverage

---
+++ tests/integration/test_custom_comparator.py
@@ -0,0 +1,83 @@
+import pytest
+
+
+@pytest.fixture
+def testcases_initial(testdir):
+    testdir.makeconftest(
+        """
+        import pytest
+        import math
+
+        from syrupy.extensions.amber import AmberSnapshotExtension
+
+        class CustomSnapshotExtension(AmberSnapshotExtension):
+            def matches(self, *, serialized_data, snapshot_data):
+                try:
+                    a = float(serialized_data)
+                    b = float(snapshot_data)
+                    return math.isclose(a, b, rel_tol=1e-5)
+                except:
+                    return False
+
+        @pytest.fixture
+        def snapshot_custom(snapshot):
+            return snapshot.use_extension(CustomSnapshotExtension)
+        """
+    )
+    return {
+        "passed": (
+            """
+            def test_passed_custom(snapshot_custom):
+                assert snapshot_custom == 3.0
+            """
+        ),
+        "failed": (
+            """
+            def test_passed_custom(snapshot_custom):
+                assert snapshot_custom == 4.0
+            """
+        ),
+    }
+
+
+@pytest.fixture
+def generate_snapshots(testdir, testcases_initial):
+    testdir.makepyfile(test_file=testcases_initial["passed"])
+    result = testdir.runpytest("-v", "--snapshot-update")
+    return result, testdir, testcases_initial
+
+
+def test_generated_snapshots(generate_snapshots):
+    result = generate_snapshots[0]
+    result.stdout.re_match_lines((r"1 snapshot generated\."))
+    assert "snapshots unused" not in result.stdout.str()
+    assert result.ret == 0
+
+
+def test_approximate_match(generate_snapshots):
+    testdir = generate_snapshots[1]
+    testdir.makepyfile(
+        test_file="""
+            def test_passed_custom(snapshot_custom):
+                assert snapshot_custom == 3.2
+            """
+    )
+    result = testdir.runpytest("-v")
+    result.stdout.re_match_lines((r"test_file.py::test_passed_custom PASSED"))
+    assert result.ret == 0
+
+
+def test_failed_snapshots(generate_snapshots):
+    testdir = generate_snapshots[1]
+    testdir.makepyfile(test_file=generate_snapshots[2]["failed"])
+    result = testdir.runpytest("-v")
+    result.stdout.re_match_lines((r"1 snapshot failed\."))
+    assert result.ret == 1
+
+
+def test_updated_snapshots(generate_snapshots):
+    _, testdir, initial = generate_snapshots
+    testdir.makepyfile(test_file=initial["failed"])
+    result = testdir.runpytest("-v", "--snapshot-update")
+    result.stdout.re_match_lines((r"1 snapshot updated\."))
+    assert result.ret == 0
