ee9ecf55fe3b0660fc33953c5a2e3f19fa18534b
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2022-08-11 16:26:09 -0400

    chore: add benchmark github action

--- tasks/benchmark.py
+++ tasks/benchmark.py
@@ -1,11 +1,10 @@
 from invoke import task
 
 
-@task(help={"report": "Publish report as github status"})
+@task()
 def benchmark(ctx, report=False):
     """
     Run and generate benchmarks for current code
     """
-    from benchmarks.main import main
 
-    main(report=report)
+    ctx.run(f"pytest benchmarks --benchmark-json=benchmarks.json")
