b657eef504cf63e63685477dab29b701a23a2679
Magnus Heskestad Waage <MHWA@equinor.com>
2022-11-08 15:36:23 +0100

    test: patch correct target

--- tests/syrupy/test_terminal.py
+++ tests/syrupy/test_terminal.py
@@ -1,21 +1,25 @@
-from unittest.mock import patch, NonCallableMock
+from unittest.mock import (
+    NonCallableMock,
+    patch,
+)
 
 import pytest
+
 from syrupy.constants import DISABLE_COLOR_ENV_VAR
 from syrupy.terminal import (
-    reset,
-    red,
-    yellow,
-    green,
     bold,
-    error_style,
-    warning_style,
-    success_style,
-    snapshot_style,
-    snapshot_diff_style,
-    received_style,
-    received_diff_style,
     context_style,
+    error_style,
+    green,
+    received_diff_style,
+    received_style,
+    red,
+    reset,
+    snapshot_diff_style,
+    snapshot_style,
+    success_style,
+    warning_style,
+    yellow,
 )
 
 
@@ -26,8 +30,10 @@ def test_colors_off_does_not_call_colored():
     see issue #633
     """
 
-    with patch('colored.colored.__init__', new_callable=NonCallableMock):
-        with patch.dict('os.environ', {DISABLE_COLOR_ENV_VAR: 'true'}):
+    with patch(
+        "syrupy.terminal.colored.colored.__init__", new_callable=NonCallableMock
+    ):
+        with patch.dict("os.environ", {DISABLE_COLOR_ENV_VAR: "true"}):
             for method in (
                 reset,
                 red,
@@ -44,9 +50,9 @@ def test_colors_off_does_not_call_colored():
                 context_style,
             ):
                 _ = method("foo")
-        
+
         # Prevent test from accidentally passing by patching wrong object
         with pytest.raises(TypeError) as excinfo:
             _ = red("foo")
-        
-        assert 'NonCallableMock' in str(excinfo.value)
+
+        assert "NonCallableMock" in str(excinfo.value)
