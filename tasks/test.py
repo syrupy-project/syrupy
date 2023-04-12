43e359165e3a25f893f4e39e2af0321128ca267f
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2023-04-12 11:24:45 -0400

    chore: remove codecov package

--- tasks/test.py
+++ tasks/test.py
@@ -46,5 +46,3 @@ def test(
     if coverage:
         if not os.environ.get("CI") or not os.environ.get("CODECOV_TOKEN"):
             ctx_run(ctx, "coverage report")
-        else:
-            ctx_run(ctx, "codecov")
