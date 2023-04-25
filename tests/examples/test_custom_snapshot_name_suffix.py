a69341d8bd91bc04e23a6b96b5f7099eb9596ab1
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2023-04-25 10:03:25 -0400

    wip

--- tests/examples/test_custom_snapshot_name_suffix.py
+++
@@ -1,3 +0,0 @@
-def test_snapshot_custom_snapshot_name_suffix(snapshot):
-    assert "Syrupy is amazing!" == snapshot(name="test_is_amazing")
-    assert "Syrupy is awesome!" == snapshot(name="test_is_awesome")
