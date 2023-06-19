f71d1aafe9fa1af0f7006c475c042e75ac4893bc
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2023-06-18 23:42:12 -0400

    fix: incorrect marking of TestClass.test_method as unused, close #717

--- tests/integration/test_single_file_multiple_extensions.py
+++ tests/integration/test_single_file_multiple_extensions.py
@@ -38,3 +38,39 @@ def test_multiple_file_extensions(testdir):
     result.stdout.re_match_lines((r"1 snapshot passed\."))
     assert "snapshots unused" not in result.stdout.str()
     assert result.ret == 0
+
+
+def test_class_style(testdir):
+    """
+    Regression test for https://github.com/tophat/syrupy/issues/717
+    """
+
+    testcase = """
+    import pytest
+    from syrupy.extensions.json import JSONSnapshotExtension
+
+    @pytest.fixture
+    def snapshot(snapshot):
+        return snapshot.use_extension(JSONSnapshotExtension)
+
+    class TestFoo:
+        def test_foo(self, snapshot):
+            assert { 'key': 'value' } == snapshot
+    """
+
+    test_file: Path = testdir.makepyfile(test_file=testcase)
+
+    result = testdir.runpytest("-v", "--snapshot-update")
+    result.stdout.re_match_lines((r"1 snapshot generated\."))
+    assert "deleted" not in result.stdout.str()
+    assert result.ret == 0
+
+    snapshot_file = (
+        Path(test_file).parent / "__snapshots__" / "test_file" / "TestFoo.test_foo.json"
+    )
+    assert snapshot_file.exists()
+
+    result = testdir.runpytest("-v")
+    result.stdout.re_match_lines((r"1 snapshot passed\."))
+    assert "snapshots unused" not in result.stdout.str()
+    assert result.ret == 0
