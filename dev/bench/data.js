86f2f1f68b113be6cc1839d947cefedfbf031e5a
github-action-benchmark <github@users.noreply.github.com>
2023-07-17 18:38:12 +0000

    add Benchmark (pytest) benchmark result for 319ac7ebdf1000c0717cc578100521d2ab223c09

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689109857963,
+  "lastUpdate": 1689619092360,
   "repoUrl": "https://github.com/tophat/syrupy",
   "entries": {
     "Benchmark": [
@@ -3960,6 +3960,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.05963585108102107",
             "extra": "mean: 1.3805044159999937 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "319ac7ebdf1000c0717cc578100521d2ab223c09",
+          "message": "chore(deps): update dependency flake8-comprehensions to v3.14.0 (#773)\n\nCo-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>",
+          "timestamp": "2023-07-17T14:36:06-04:00",
+          "tree_id": "8606f348cfff51b8ac2e2b8a60323b579d741d8d",
+          "url": "https://github.com/tophat/syrupy/commit/319ac7ebdf1000c0717cc578100521d2ab223c09"
+        },
+        "date": 1689619090600,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/test_1000x.py::test_1000x_reads",
+            "value": 0.5623892551866011,
+            "unit": "iter/sec",
+            "range": "stddev: 0.10755670860936692",
+            "extra": "mean: 1.7781278550000024 sec\nrounds: 5"
+          },
+          {
+            "name": "benchmarks/test_1000x.py::test_1000x_writes",
+            "value": 0.5505784512761549,
+            "unit": "iter/sec",
+            "range": "stddev: 0.08041569534240654",
+            "extra": "mean: 1.8162715915999912 sec\nrounds: 5"
+          },
+          {
+            "name": "benchmarks/test_standard.py::test_standard",
+            "value": 0.575505035062185,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0845423510553044",
+            "extra": "mean: 1.7376042590000054 sec\nrounds: 5"
           }
         ]
       }
