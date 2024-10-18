27f535dcbcbadac5222c10ac42b7e57ee4b177c6
epenet <6771947+epenet@users.noreply.github.com>
2024-10-18 10:28:38 +0200

    adjust tests

--- tests/integration/test_snapshot_similar_names_file_extension.py
+++ tests/integration/test_snapshot_similar_names_file_extension.py
@@ -41,45 +41,45 @@ def run_testcases(testdir, testcases):
     return testdir, testcases
 
 
-def test_run_all(run_testcases, plugin_args_fails_xdist):
+def test_run_all(run_testcases, plugin_args):
     testdir, testcases = run_testcases
     result = testdir.runpytest(
         "-v",
         "--snapshot-default-extension",
         "syrupy.extensions.single_file.SingleFileSnapshotExtension",
-        *plugin_args_fails_xdist,
+        *plugin_args,
     )
     result.stdout.re_match_lines("9 snapshots passed")
     assert result.ret == 0
 
 
-def test_run_single_file(run_testcases, plugin_args_fails_xdist):
+def test_run_single_file(run_testcases, plugin_args):
     testdir, testcases = run_testcases
     result = testdir.runpytest(
         "-v",
         "--snapshot-default-extension",
         "syrupy.extensions.single_file.SingleFileSnapshotExtension",
         "test_1.py",
-        *plugin_args_fails_xdist,
+        *plugin_args,
     )
     result.stdout.re_match_lines("3 snapshots passed")
     assert result.ret == 0
 
 
-def test_run_single_test_case_in_file(run_testcases, plugin_args_fails_xdist):
+def test_run_single_test_case_in_file(run_testcases, plugin_args):
     testdir, testcases = run_testcases
     result = testdir.runpytest(
         "-v",
         "--snapshot-default-extension",
         "syrupy.extensions.single_file.SingleFileSnapshotExtension",
         "test_2.py::test_a",
-        *plugin_args_fails_xdist,
+        *plugin_args,
     )
     result.stdout.re_match_lines("1 snapshot passed")
     assert result.ret == 0
 
 
-def test_run_all_but_one(run_testcases, plugin_args_fails_xdist):
+def test_run_all_but_one(run_testcases, plugin_args):
     testdir, testcases = run_testcases
     result = testdir.runpytest(
         "-v",
@@ -88,13 +88,13 @@ def test_run_all_but_one(run_testcases, plugin_args_fails_xdist):
         "syrupy.extensions.single_file.SingleFileSnapshotExtension",
         "test_1.py",
         "test_2.py::test_a",
-        *plugin_args_fails_xdist,
+        *plugin_args,
     )
     result.stdout.re_match_lines("4 snapshots passed")
     assert result.ret == 0
 
 
-def test_run_both_files_by_node(run_testcases, plugin_args_fails_xdist):
+def test_run_both_files_by_node(run_testcases, plugin_args):
     testdir, testcases = run_testcases
     result = testdir.runpytest(
         "-v",
@@ -103,13 +103,13 @@ def test_run_both_files_by_node(run_testcases, plugin_args_fails_xdist):
         "syrupy.extensions.single_file.SingleFileSnapshotExtension",
         "test_1.py::test_a",
         "test_2.py::test_a",
-        *plugin_args_fails_xdist,
+        *plugin_args,
     )
     result.stdout.re_match_lines("2 snapshots passed")
     assert result.ret == 0
 
 
-def test_run_both_files_by_node_2(run_testcases, plugin_args_fails_xdist):
+def test_run_both_files_by_node_2(run_testcases, plugin_args):
     testdir, testcases = run_testcases
     result = testdir.runpytest(
         "-v",
@@ -118,7 +118,7 @@ def test_run_both_files_by_node_2(run_testcases, plugin_args_fails_xdist):
         "syrupy.extensions.single_file.SingleFileSnapshotExtension",
         "test_1.py::test_b",
         "test_2.py::test_a",
-        *plugin_args_fails_xdist,
+        *plugin_args,
     )
     result.stdout.re_match_lines("2 snapshots passed")
     assert result.ret == 0
--- tests/integration/test_xfail.py
+++ tests/integration/test_xfail.py
@@ -1,4 +1,4 @@
-def test_no_failure_printed_if_all_failures_xfailed(testdir, plugin_args):
+def test_no_failure_printed_if_all_failures_xfailed(testdir, plugin_args_fails_xdist):
     testdir.makepyfile(
         test_file=(
             """
@@ -10,7 +10,7 @@ def test_no_failure_printed_if_all_failures_xfailed(testdir, plugin_args):
         """
         )
     )
-    result = testdir.runpytest("-v", *plugin_args)
+    result = testdir.runpytest("-v", *plugin_args_fails_xdist)
     result.stdout.no_re_match_line((r".*snapshot failed*"))
     assert result.ret == 0
 
@@ -38,7 +38,7 @@ def test_failures_printed_if_only_some_failures_xfailed(
     assert result.ret == 1
 
 
-def test_failure_printed_if_xfail_does_not_run(testdir, plugin_args_fails_xdist):
+def test_failure_printed_if_xfail_does_not_run(testdir, plugin_args):
     testdir.makepyfile(
         test_file=(
             """
@@ -50,7 +50,7 @@ def test_failure_printed_if_xfail_does_not_run(testdir, plugin_args_fails_xdist)
         """
         )
     )
-    result = testdir.runpytest("-v", *plugin_args_fails_xdist)
+    result = testdir.runpytest("-v", *plugin_args)
     result.stdout.re_match_lines((r".*1 snapshot failed*"))
     result.stdout.no_re_match_line((r".*1 snapshot xfailed*"))
     assert result.ret == 1
