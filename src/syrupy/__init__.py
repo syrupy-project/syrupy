7f7f6dd088d45544b695886224436de7f299528b
T Sparrow <793763+sparrowt@users.noreply.github.com>
2025-02-20 10:13:39 +0000

    fix: __terminal_color shouldn't modify os.environ unless it has to
    See https://github.com/syrupy-project/syrupy/issues/955 for a
    detailed explanation of why this can cause thread safety issues
    resulting in a segfault when another thread calls `getenv`

--- src/syrupy/__init__.py
+++ src/syrupy/__init__.py
@@ -1,4 +1,5 @@
 import argparse
+import contextlib
 import sys
 from functools import lru_cache
 from gettext import gettext
@@ -105,11 +106,14 @@ def pytest_addoption(parser: "pytest.Parser") -> None:
 
 
 def __terminal_color(config: "pytest.Config") -> "ContextManager[None]":
-    env = {}
     if config.option.no_colors:
-        env[DISABLE_COLOR_ENV_VAR] = "true"
-
-    return env_context(**env)
+        env = {
+            DISABLE_COLOR_ENV_VAR: "true",
+        }
+        return env_context(**env)
+    else:
+        # No-op to avoid unnecessary env updates
+        return contextlib.nullcontext()
 
 
 def pytest_assertrepr_compare(
