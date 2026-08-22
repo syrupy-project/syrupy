d618a625770ef99d9473c3a3335aac8fe9a4dc00
github-action-benchmark <github@users.noreply.github.com>
2026-08-22 16:41:47 +0000

    add Benchmark (pytest) benchmark result for a5ce0492e68525f499f5cb395e273d3ac5a1f8e3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787416782849,
+  "lastUpdate": 1787416907049,
   "repoUrl": "https://github.com/syrupy-project/syrupy",
   "entries": {
     "Benchmark": [
@@ -18191,6 +18191,58 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.4190102176403809",
             "extra": "mean: 1.1990468157999985 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a5ce0492e68525f499f5cb395e273d3ac5a1f8e3",
+          "message": "chore(deps): update actions/configure-pages action to v6 (#1219)\n\nCo-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>",
+          "timestamp": "2026-08-22T12:40:36-04:00",
+          "tree_id": "458aab59bbccb6d32e0ed709b2a7027dc1e43ecb",
+          "url": "https://github.com/syrupy-project/syrupy/commit/a5ce0492e68525f499f5cb395e273d3ac5a1f8e3"
+        },
+        "date": 1787416906479,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/test_1000x.py::test_1000x_reads",
+            "value": 0.8191825656223992,
+            "unit": "iter/sec",
+            "range": "stddev: 0.08363206116243105",
+            "extra": "mean: 1.2207291048000015 sec\nrounds: 5"
+          },
+          {
+            "name": "benchmarks/test_1000x.py::test_1000x_writes",
+            "value": 0.7341029510791374,
+            "unit": "iter/sec",
+            "range": "stddev: 0.21681922262330955",
+            "extra": "mean: 1.3622067565999998 sec\nrounds: 5"
+          },
+          {
+            "name": "benchmarks/test_partial_selection.py::test_partial_selection_teardown",
+            "value": 2.222585489497154,
+            "unit": "iter/sec",
+            "range": "stddev: 0.002840206326261122",
+            "extra": "mean: 449.92645039999957 msec\nrounds: 5"
+          },
+          {
+            "name": "benchmarks/test_standard.py::test_standard",
+            "value": 0.7334090241369347,
+            "unit": "iter/sec",
+            "range": "stddev: 0.3708785064841999",
+            "extra": "mean: 1.363495630799997 sec\nrounds: 5"
           }
         ]
       }
