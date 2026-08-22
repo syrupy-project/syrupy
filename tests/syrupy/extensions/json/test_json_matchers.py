afc1a9a4a89d94174c82e02414bf6df1ad03781d
Noah <noahnu@gmail.com>
2026-08-22 11:50:48 -0400

    fix(#1058): add date serialization (#1216)
    Co-authored-by: Daniel Nehrig <daniel.nehrig@centricsoftware.com>

--- tests/syrupy/extensions/json/test_json_matchers.py
+++ tests/syrupy/extensions/json/test_json_matchers.py
@@ -15,10 +15,10 @@ def snapshot_json(snapshot):
 def test_matcher(snapshot_json):
     content = {
         "int": random.randint(1, 100),
-        "date": datetime.datetime.utcnow(),
+        "date": datetime.datetime.now(datetime.timezone.utc),
         "foo": {
             "x": "y",
-            "another_date": datetime.datetime.utcnow(),
+            "another_date": datetime.datetime.now(datetime.timezone.utc),
         },
     }
     matcher = path_type(
